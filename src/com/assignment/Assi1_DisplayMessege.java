f217ee5f035c20eebe3c0feaaf7f1879314127a1
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-14 19:59:21 +0530

    Display any messege

---
+++ src/com/assignment/Assi1_DisplayMessege.java
@@ -0,0 +1,12 @@
+package com.assignment;
+
+public class Assi1_DisplayMessege {
+	
+		public static void main(String [] args)
+		{
+			System.out.println("Hello I am BridgelabStudent");
+		}
+
+	}
+
+

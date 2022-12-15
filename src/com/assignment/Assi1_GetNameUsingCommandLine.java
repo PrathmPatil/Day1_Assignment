0184e08bac373cf4ef21a9196f05e8ede74cc378
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-15 09:46:48 +0530

    Get name using command line

---
+++ src/com/assignment/Assi1_GetNameUsingCommandLine.java
@@ -0,0 +1,16 @@
+package com.assignment;
+
+public class Assi1_GetNameUsingCommandLine {
+	
+		public static void main(String[] args) {
+			 System.out.println("This is a simple command line program!");
+			    System.out.println("Enter Name:"); 
+
+			    for(String str: args) {
+			    	System.out.println(str);
+			    }
+		}
+
+	}
+
+

49655fcb855134ed108a6d51a411fde631e9be63
PrathmPatil <prathmpatil2818@gmail.com>
2022-12-15 09:38:19 +0530

    Chekiing Strings are same or not

---
+++ src/com/assignment/Assi1_EqualityOfString.java
@@ -0,0 +1,23 @@
+package com.assignment;
+
+public class Assi1_EqualityOfString {
+
+		public static void main(String[] args) {
+		String val1="i am Prathm";
+		String val2="I am Prathm";
+		
+		if(val1==val2)
+		{
+			System.out.println("both are same");
+		}
+		else
+		{
+			System.out.println("both are not same");
+		}
+		
+			
+		}
+
+	}
+
+
